b333570a9ff3b8b2e9c6cdb7d981c0d172691bf6
Naoki Mita <naoki.mita18@gmail.com>
2020-02-27 14:42:50 -0800

    Finish getNumberOfTown quiz

--- SwiftAlgorithm/UnionFind/FloodFill/getNumberOfTown.swift
+++ SwiftAlgorithm/UnionFind/FloodFill/getNumberOfTown.swift
@@ -2,8 +2,49 @@
 //  getNumberOfTown.swift
 //  SwiftAlgorithm
 //
-//  Created by 美田直毅 on 2020-02-27.
+//  Created by Naoki Mita on 2020-02-27.
 //  Copyright © 2020 Naoki Mita. All rights reserved.
 //
 
 import Foundation
+
+func getNumberOfTown() {
+    // create input
+    let n: Int = Int(readLine()!)!
+    var array: [[Int]] = [[Int]](repeating: [Int](repeating: 0, count: n), count: n)
+    for i in 0..<n {
+        let row: String = readLine()!
+        for j in 0..<row.count {
+            array[i][j] = Int(row[j])!
+        }
+    }
+
+    func dfs(row: Int, col: Int, visited: inout [[Int]]) -> Int {
+        guard row > -1 && row < n else { return 0 }
+        guard col > -1 && col < n else { return 0 }
+        guard visited[row][col] == 1 else { return 0 }
+        
+        visited[row][col] = 0
+        var sum = 1
+        sum += dfs(row: row - 1, col: col, visited: &visited)
+        sum += dfs(row: row, col: col + 1, visited: &visited)
+        sum += dfs(row: row + 1, col: col, visited: &visited)
+        sum += dfs(row: row, col: col - 1, visited: &visited)
+        
+        return sum
+    }
+    
+    var res: [Int] = []
+    for i in 0..<n {
+        for j in 0..<n {
+            if array[i][j] == 1 {
+                res.append(dfs(row: i, col: j, visited: &array))
+            }
+        }
+    }
+    res.sort()
+    print(res.count)
+    for r in res {
+        print(r)
+    }
+}
