62612cd4d8a1480c9e5dcb8a301da75411004bae
Naoki Mita <naoki.mita18@gmail.com>
2020-02-27 14:42:18 -0800

    Add testcases for lca

---
+++ SwiftAlgorithm/UnionFind/FloodFill/getNumberOfIslands.swift
@@ -0,0 +1,9 @@
+//
+//  getNumberOfIslands.swift
+//  SwiftAlgorithm
+//
+//  Created by 美田直毅 on 2020-02-27.
+//  Copyright © 2020 Naoki Mita. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftAlgorithm/UnionFind/FloodFill/getNumberOfTown.swift
@@ -0,0 +1,9 @@
+//
+//  getNumberOfTown.swift
+//  SwiftAlgorithm
+//
+//  Created by 美田直毅 on 2020-02-27.
+//  Copyright © 2020 Naoki Mita. All rights reserved.
+//
+
+import Foundation
